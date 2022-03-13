0b6daa8a82f7adfe4d1f72f93f0e75145806629b
Hiroaki Matsuyama <hmatsuya@gmail.com>
2022-03-13 17:00:56 +0900

    Specified package name in setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name='usi_engine', version='0.0.2', install_requires=['cshogi',])
+setup(name='usi_engine', version='0.0.2', install_requires=['cshogi',], packages=['usi_engine',])
